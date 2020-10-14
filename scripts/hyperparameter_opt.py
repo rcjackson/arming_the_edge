182e2f28a00339a5ce43ea597299c18ffa58eaf7
Robert Jackson <rjackson@lambda1.cels.anl.gov>
2020-10-14 12:16:22 -0700

    K

--- scripts/hyperparameter_opt.py
+++ scripts/hyperparameter_opt.py
@@ -2,52 +2,137 @@ import xarray as xr
 import numpy as np
 import pandas as pd
 
-from dask_jobqueue import SLURMCluster
-from distributed import Client
-from xgboost import XGBClassifier
-from dask_ml.model_selection import GridSearchCV
+#from distributed import Client
+import xgboost as xgb
+from hyperopt import hp, fmin, tpe, space_eval, Trials, STATUS_OK
+#from hyperopt.mongoexp import MongoTrials
+from datetime import datetime
 
+label_df = pd.read_csv('../notebooks/lidar_labels.csv')
+
+date_list = np.array([datetime.strptime(x, '%Y-%m-%d').date() for x in label_df["Date"].values])
+start_time_list = np.array([datetime.strptime(x[0:4], '%H%M').time() for x in label_df["Time"].values])
+end_time_list = np.array([datetime.strptime(x[5:], '%H%M').time() for x in label_df["Time"].values])
+
+def get_label(dt):
+    label_ind = np.where(np.logical_and.reduce(
+        (date_list == dt.date(), start_time_list <= dt.time(), end_time_list > dt.time())))
+    if not label_ind[0].size:
+        return -1
+    my_strings = np.array(
+            [x.lower() for x in label_df["Label"].values[label_ind]])
+    num_cloud = len(np.where(my_strings == "cloudy")[0]) + \
+            len(np.where(my_strings == "cloud")[0])
+    num_clear = len(np.where(my_strings == "clear")[0])
+    num_rain = len(np.where(my_strings == "rain")[0])
+    my_string = label_df["Label"].values[label_ind][0]
+ 
+    pct_cloud = num_cloud/len(my_strings)
+    
+    if num_rain > 0:
+        return 3
+    elif pct_cloud < 0.25:
+        return 0
+    elif pct_cloud >= 0.25 and pct_cloud <= 0.75:
+        return 1
+    else:
+        return 2
+    
+    raise ValueError("Invalid value %s for label" % my_string)
+
+def dt64_to_dt(dt):
+    ts = (dt - np.datetime64('1970-01-01T00:00:00Z')) / np.timedelta64(1, 's')
+    return datetime.utcfromtimestamp(ts)
 
 def main():
-    my_ds = xr.open_dataset('../notebooks/encoded_snr_parameters.nc')
-    encoded_data_test = my_ds.encoded_data_test.values
-    encoded_data_train = my_ds.encoded_data_train.values
-    test_labels = my_ds.test_labels
-    train_labels = my_ds.train_labels
+    my_ds = xr.open_mfdataset('/lambda_stor/data/rjackson/coverage_product/*.nc')
+    labels = my_ds['time'].values
+    labels = np.array([get_label(dt64_to_dt(x)) for x in labels])
 
-    nonans_test = ~np.isnan(test_labels)
-    nonans_train = ~np.isnan(train_labels)
-    encoded_data_test = encoded_data_test[nonans_test, :]
-    encoded_data_train = encoded_data_train[nonans_train, :]
-    test_labels = test_labels[nonans_test]
-    train_labels = train_labels[nonans_train]
+    feature_list = ['snrgt1.000000', 'snrgt3.000000', 'snrgt5.000000']
+
+    x = np.concatenate([my_ds[x].values for x in feature_list], axis=1)
+    valid = np.where(labels > -1)[0]
+    x = x[valid, :]
+    labels = labels[valid]
+
+    feature_labels = []
+    for feat in feature_list:
+        for i in range(len(my_ds.range_bins.values)):
+            feature_labels.append('%s%d' % (feat, my_ds.range_bins[i])) 
 
     print("Submitting jobs to queue...")
-    my_cluster = SLURMCluster(memory='64GB', cores=36, walltime='4:00:00', processes=6)
-    my_cluster.scale(32)
-    client = Client(my_cluster)
+    
     
     print("Waiting for cluster...")
-    while client.ncores() == {}:
-        continue
-    print(client)
-    print(my_cluster)
+    #while client.ncores() == {}:
+    #    continue
+    #print(client)
+    #print(my_cluster)
     print("Performing hyperparameter optimization...")
 
-    my_classifier = XGBClassifier(n_jobs=6)
-    params = {'learning_rate': np.arange(0.01, 0.2, 0.1),
-              'max_depth': np.arange(1, 20, 1, dtype='int'),
-              'n_estimators': np.arange(10, 1000, 50, dtype='int')}
-    scorer = GridSearchCV(estimator=my_classifier, param_grid=params)
-    scorer.fit(encoded_data_train, train_labels)    
+    #my_classifier = XGBClassifier(n_jobs=6, n_estimators=1000)
+    #params = {'max_depth': 7,
+    #          'colsample_bytree': 1,
+    #          'subsample': 0.9,
+    #          'gpu_id': 1,
+    #          'eta': 0.01,
+    #          'num_parallel_tree': 1,
+    #          'tree_method': 'gpu_hist',
+    #          'objective': 'multi:softmax',
+    #          'num_class': 3,
+    #          'verbosity': 1}
 
-    print(scorer.cv_results_)
-    print(scorer.cv_results_['params'][scorer.best_index_])
-    results_df = pd.DataFrame(scorer.cv_results_)
-    results_dt.to_csv('hyperparameter_opt.csv')             
+    params = {'eta': hp.uniform('eta', 0.01, 1),
+              'max_depth': hp.choice('max_depth', [3, 4, 5, 6, 7, 8, 9, 10, 11]),
+              'num_rounds': hp.choice('num_rounds', 
+                  [500, 1000, 1500, 2000, 2500, 3000]),
+              'gamma': hp.uniform('gamma', 0, 10),
+              'colsample_bytree': hp.uniform('colsample_bytree', 0.4, 1),
+              'subsample': hp.uniform('subsample', 0.6, 1),
+              }
+              
+    #dtrain = xgb.DMatrix(x_train, label=y_train, feature_names=feature_labels)
+    #dtest = xgb.DMatrix(x_test, label=y_test, feature_names=feature_labels)
+    dall = xgb.DMatrix(x, label=labels, feature_names=feature_labels)
+    num_rounds = 10000
+
+    def lr(boosting_round, num_boost_round):
+        return max([0.0001, 0.1 - 0.1*boosting_round/num_boost_round])
     
-    my_cluster.close()
-    del client
+    print("Training...")
+    gpu_res = {}
+    def objective(x):
+        params = {'eta': x['eta'],
+                  'max_depth': x['max_depth'],
+                  'num_parallel_tree': 1,
+                  'objective': 'multi:softmax',
+                  'num_class': 4,
+                  'tree_method': 'gpu_hist',
+                  'gpu_id': 1,
+                  'colsample_bytree': x['colsample_bytree'],
+                  'subsample': x['subsample'],
+                  'gamma': x['gamma'],
+                  'verbosity': 0}
+
+        res = xgb.cv(params, dall, nfold=5,
+                     num_boost_round=x['num_rounds'],
+                     callbacks=[xgb.callback.print_evaluation(1)])
+        return {'loss': res['test-merror-mean'].min(), 'status': STATUS_OK}
+
+    trials = Trials()
+    best = fmin(objective, space=params, algo=tpe.suggest, max_evals=1000,
+             trials=trials) 
+    print(best)
+
+    #my_classifier.fit(x, labels)
+    #print(scorer.cv_results_)
+    #print(scorer.cv_results_['params'][scorer.best_index_])
+    #results_df = pd.DataFrame(scorer.cv_results_)
+    #results_dt.to_csv('hyperparameter_opt.csv')             
+    
+    #my_cluster.close()
+    #del client
     
 if __name__ == '__main__':
     main()
--- scripts/preprocess_lidar_moments.py
+++ scripts/preprocess_lidar_moments.py
@@ -13,16 +13,45 @@ import xarray as xr
 import tensorflow as tf
 import os
 import glob
+import pandas as pd
 
-tfrecords_path = '/nfs/gce/projects/digr/lidar_tfrecords/'
-lidar_files = "/run/user/7920/gvfs/sftp:host=bebop.lcrc.anl.gov/lcrc/group/earthscience/rjackson/sgp_lidar/processed_moments/sgpdlacfC1.a1*moments.nc"
+from datetime import datetime
+
+tfrecords_path = '/lambda_stor/data/rjackson/lidar_tfrecords/velocity/'
+lidar_files = "/lambda_stor/data/rjackson/sgp_lidar/*moments*.nc"
 first_shape = None
+label_df = pd.read_csv('../notebooks/lidar_labels.csv')
+
+date_list = np.array([datetime.strptime(x, '%Y-%m-%d').date() for x in label_df["Date"].values])
+start_time_list = np.array([datetime.strptime(x[0:4], '%H%M').time() for x in label_df["Time"].values])
+end_time_list = np.array([datetime.strptime(x[5:], '%H%M').time() for x in label_df["Time"].values])
+
+def get_label(dt):
+    label_ind = np.where(np.logical_and.reduce(
+        (date_list == dt.date(), start_time_list <= dt.time(), end_time_list > dt.time())))
+    if not label_ind[0].size:
+        return -1
+    my_string = label_df["Label"].values[label_ind][0]
+
+    if my_string.lower() == 'clear':
+        return 0
+    elif my_string.lower() == 'cloudy' or my_string.lower() == "cloud":
+        return 1
+    elif my_string.lower() == 'rain':
+        return 2
+
+    raise ValueError("Invalid value %s for label" % my_string)
+
+
+def dt64_to_dt(dt):
+    ts = (dt - np.datetime64('1970-01-01T00:00:00Z')) / np.timedelta64(1, 's')
+    return datetime.utcfromtimestamp(ts)
 
 def create_tf_record(file, time_interval=5, first_shape=None):
     # Get radar file from bebop
 
     grid = xr.open_dataset(file)
-    Zn = grid.snr.fillna(0).values
+    Zn = grid.snr.where(grid.snr > 1).fillna(0).values
     Zn = Zn/30
     Zn = np.where(Zn > 1, 1, Zn)
 
@@ -66,15 +95,19 @@ def create_tf_record(file, time_interval=5, first_shape=None):
         width = first_shape[0]
         height = first_shape[1]
         # norm = norm.SerializeToString()
-        example = tf.train.Example(features=tf.train.Features(
-            feature={
-                'width': _int64_feature(width),
-                'height': _int64_feature(height),
-                'image_raw': _bytes_feature(my_data),
-                'start_time': _float_feature(my_times[0]),
-                'end_time': _float_feature(my_times[-1])
-            }))
-        writer.write(example.SerializeToString())
+
+        label = get_label(dt64_to_dt(my_times[0]))
+        if label > -1:
+            example = tf.train.Example(features=tf.train.Features(
+                feature={
+                    'width': _int64_feature(width),
+                    'height': _int64_feature(height),
+                    'image_raw': _bytes_feature(my_data),
+                    'start_time': _float_feature(my_times[0]),
+                    'end_time': _float_feature(my_times[-1]),
+                    'label': _int64_feature(label)
+                }))
+            writer.write(example.SerializeToString())
         i += 1
     return first_shape
 
