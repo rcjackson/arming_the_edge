449729dea60fe1058a936d3f7683a99e74d13362
Robert Jackson <rjackson@lambda1.cels.anl.gov>
2022-01-04 11:38:17 -0800

    ADD: Newest notebooks

--- scripts/make_scp.py
+++ scripts/make_scp.py
@@ -12,6 +12,7 @@ out_path = '/lambda_stor/data/rjackson/coverage_product/'
 
 range_bins = np.arange(0., 12000., 60)
 snr_bins = np.arange(1., 51, 1.)
+vel_bins = np.arange(-20., 20., 0.5)
 
 for f in file_list:
     print(f)
@@ -25,18 +26,27 @@ for f in file_list:
         for t in thresholds:
             arr = np.zeros((19, len(range_bins) - 1))
             hist_array = np.zeros((19, len(range_bins) - 1, len(snr_bins) - 1))
+            hist_array_vel = np.zeros((19, len(range_bins) - 1, len(vel_bins) - 1))
 
             for i in range(19):
                 time_inds = np.argwhere(np.logical_and(inp_ds.time.values >= time_range[i],
                     inp_ds.time.values < time_range[i+1]))
                 
                 hist_array[i] = np.squeeze(np.apply_along_axis(
-                        lambda a: np.histogram(a, bins=snr_bins, normed=False)[0],
+                        lambda a: np.histogram(a, bins=snr_bins)[0],
                         0, inp_ds['snr'].values[time_inds, 1:-1:2])).T
                 hist_array[i] = hist_array[i] + \
                         np.squeeze(np.apply_along_axis(
-                        lambda a: np.histogram(a, bins=snr_bins, normed=False)[0],
+                        lambda a: np.histogram(a, bins=snr_bins)[0],
                         0, inp_ds['snr'].values[time_inds, 0:-2:2])).T
+                hist_array_vel[i] = np.squeeze(np.apply_along_axis(
+                        lambda a: np.histogram(a, bins=vel_bins)[0],
+                        0, inp_ds['mean_velocity'].values[time_inds, 1:-1:2])).T
+                hist_array_vel[i] = hist_array_vel[i] + \
+                        np.squeeze(np.apply_along_axis(
+                        lambda a: np.histogram(a, bins=vel_bins)[0],
+                        0, inp_ds['mean_velocity'].values[time_inds, 0:-2:2])).T
+                
                 if len(time_inds) > 1:
                     mask = np.squeeze(np.where(inp_ds[v].values[time_inds, :] > t, 1, 0))
                 elif len(time_inds) == 0:
@@ -52,12 +62,13 @@ for f in file_list:
             scp_ds['%sgt%1.6f' % (v, t)].attrs["long_name"] = "Statistical coverage of %s > %f" % (v, t)
             scp_ds['snr_hist'] = xr.DataArray(hist_array, 
                     dims=('time', 'range', 'snr_bins'))
+            scp_ds['vel_hist'] = xr.DataArray(hist_array_vel, 
+                    dims=('time', 'range', 'vel_bins'))
     inp_ds.close()
     scp_ds['Time'] = xr.DataArray(time_range[:-1], dims=('time'))
     scp_ds['range_bins'] = xr.DataArray(range_bins[:-1], dims=('range'))
     scp_ds['snr_bins'] = xr.DataArray(snr_bins[:-1], dims=('snr_bins'))
+    scp_ds['vel_bins'] = xr.DataArray(vel_bins[:-1], dims=('vel_bins'))
     scp_ds = xr.Dataset(scp_ds)
     print(scp_ds.range_bins)
     scp_ds.to_netcdf(out_path + 'sgpdlscp.%s.%s.nc' % (ymd, hms))
-
-           
